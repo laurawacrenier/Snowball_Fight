7a738aeb7a7be5b6881d46db53290c961d5faf9f
Christian Seremetis <christianseremetis@gmail.com>
2018-08-18 23:12:31 +0000

    placed events in prototype

--- scripts/Opponent.js
+++ scripts/Opponent.js
@@ -1,33 +1,56 @@
 /** Counter to keep track of opponent ids. */
-let counter = 0;
+let COUNTER = 0;
+/** The part of the DOM where we add opponents. */
+let FIELD = document.querySelector("#opponent_field");
 
 /** Public constructor for Opponent objects. */
-function Opponent() {
+function Opponent(position) {
   //set health to starting value 10
   this.health = 10;
-  counter++;
-  this.id = counter;
+  COUNTER++;
+  this.id = COUNTER;
   //store the character's associated tag in an instance variable.
-  let location = '17.45 2.3 10.986';
-  let url = 'https://poly.google.com/view/2WOXO0LlL0K';
-  this.tag = `<a-entity class='opponent' id='opponent${this.id}' 
-  static-body gblock='${url}' position='${location}' 
-  scale='0.8 0.8 0.8'></a-entity>`;
+  let url = 'https://poly.google.com/view/5v5j_lqOHTO';
+  this.tag = `<a-entity follow='speed: 1; target:#player' class='opponent' 
+  id='opponent${this.id}' static-body gblock='${url}' position='${position}' 
+  scale='0.3 0.3 0.3'></a-entity>`;
 }
 
-/** Executes when the opponent gets hit by a snowball. */
-Opponent.prototype.damage = function() {
+/** Executes when the opponent gets hit by a snowball. 
+  * @param e {event} the handler for collisions with the opponent
+  */
+Opponent.prototype.damage = function(e) {
+  console.log('hit');
+  let healthBar = document.querySelector('#content');
   this.health--;
-};
-
-/** Makes the health appear on the screen in an "HP Bar." */
-Opponent.prototype.showHealth = function() {
-    
+  if (this.health >= 1) {
+    healthBar.innerHTML = `<h3>Opponent Health: ${this.health}</h3>`;
+  } else {
+    healthBar.innerHTML = `<h3>Opponent Defeated</h3>`;
+    this.deactivate(e.target.object3D.position);
+  }
 };
 
 /** Appends the opponent to the DOM. */
 Opponent.prototype.appear = function() {
   //make opponent appear on the DOM
-  let field = document.querySelector("#opponent_field");
-  field.innerHTML += this.tag;
+  FIELD.innerHTML += this.tag;
+  let htmlObject = document.querySelector(`#opponent${this.id}`);
+  //make it possible to collide with the opponent
+  htmlObject.addEventListener('collide', e => {
+    if (e.detail.body.el.classList.contains('snowball')) {
+      this.damage(e);
+    } else if (e.detail.body.el.classList.contains('player') 
+      && this.health >0) {
+      alert('Game Over!');
+      location.reload();
+    }
+  })
 };
+
+/** Ends a defeated Opponent's movement and danger. */
+Opponent.prototype.deactivate = function(position) {
+  let htmlObject = document.querySelector(`#opponent${this.id}`);
+  htmlObject.removeAttribute('follow');
+  this.tag = htmlObject.outerHTML;
+}
